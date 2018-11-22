31a8b244b53a793a5b6bf9c9ac7127f03d785f84
birorobert <biro.k.robert@gmail.com>
2018-11-22 10:25:05 +0100

    proba

--- gittie.py
+++ gittie.py
@@ -1,2 +1,12 @@
 def introduce():
     print("Hello, I'm Gittie")
+introduce()
+
+
+def joke():
+    szam = input("nyomj egy szamot, leginkabb az 1-t")
+    if szam == '1':
+        print("kacaghatsz")
+    else:
+        print('miért nem nyomtal 1-t?')
+joke()
